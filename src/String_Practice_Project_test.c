5f0d96c09189b037fe1b9bbe16fecdcb6c4ac8ae
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-19 17:42:57 -0500

    Able to read in a string.

--- src/String_Practice_Project_test.c
+++ src/String_Practice_Project_test.c
@@ -12,7 +12,13 @@
 #include <stdlib.h>
 
 int main(void) {
+	char prompt[] = "Greetings Human!";
+	char first[300];
+	printf(prompt);
 	puts("Greetings, Human!"); /* prints Greetings, Human! */
 	puts("Please enter 3 sentances seperated by period and end with an enter strike");
+	gets(first);
+	//scanf("%300s", first);
+	printf("Thank you for your three sentances, %s.\n", first);
 	return EXIT_SUCCESS;
 }
