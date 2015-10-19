45152f5a554ba4764fdaa0bdb4f9f911f49e9b58
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-19 13:15:11 -0500

    Starting to work with the test file

---
+++ src/String_Practice_Project_test.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : String_Practice_Project.c
+ Author      : Jeremy Evert
+ Version     :
+ Copyright   : BSD
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Greetings, Human!"); /* prints Greetings, Human! */
+	puts("Please enter 3 sentances seperated by period and end with an enter strike");
+	return EXIT_SUCCESS;
+}
