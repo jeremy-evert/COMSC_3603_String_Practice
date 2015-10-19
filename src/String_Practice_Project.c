9dbee655488074b7a092f743e300ad23082bffa2
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-19 12:38:54 -0500

    first commit

---
+++ src/String_Practice_Project.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : String_Practice_Project.c
+ Author      : Jeremy
+ Version     :
+ Copyright   : BSD
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Greetings, Human!"); /* prints Greetings, Human! */
+	return EXIT_SUCCESS;
+}
